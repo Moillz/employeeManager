1decb333f077ae0119ac8abbe5fabe68103dce70
Moillz <dai1951490147@outlook.con>
2020-02-07 15:28:19 +0800

    vue2.0+firebase

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav>
+    <div class="nav-wrapper green">
+      <div class="container">
+        <router-link to="/"
+        class="brand-logo">Emlpoyee manager
+        </router-link>
+     </div>
+    </div>
+  </nav>
+</template>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+   apiKey: "AIzaSyA77CsK8zUUqrBW1lyT3Z-BQ-bELsB14IQ",
+  authDomain: "vuefs-8c8c5.firebaseapp.com",
+  databaseURL: "https://vuefs-8c8c5.firebaseio.com",
+  projectId: "vuefs-8c8c5",
+  storageBucket: "vuefs-8c8c5.appspot.com",
+  messagingSenderId: "166462473435",
+  appId: "1:166462473435:web:9751cb4e0636a1578bf8fe",
+  measurementId: "G-NKYH4R5F30"
+}
